217da4d4c80b919f6c24cd2ce94f31e29fad9750
Miguel <miguellv2005@gmail.com>
2025-10-30 03:33:49 +0100

    Versión inicial del portafolio en pruebas (Astro + React)

---
+++ src/components/ReactComponents/MiBoton.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function MiBoton() {
+  return (
+    <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+      ¡Haz clic aquí!
+    </button>
+  );
+}

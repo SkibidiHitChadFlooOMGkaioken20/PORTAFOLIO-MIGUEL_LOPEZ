a84810c8e37edb3047e2422824da9eada5550f5b
SkibidiHitChadFlooOMGkaioken20 <miguellv2005@gmail.com>
2025-11-26 18:25:04 +0100

    Build para GitHub Pages

---
+++ dist/_astro/MiBoton.Ct0fdAzj.js
@@ -0,0 +1,9 @@
+import"./index.BgwVLlj0.js";var i={exports:{}},e={};/**
+ * @license React
+ * react-jsx-runtime.production.js
+ *
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var o;function d(){if(o)return e;o=1;var a=Symbol.for("react.transitional.element"),l=Symbol.for("react.fragment");function s(R,r,t){var u=null;if(t!==void 0&&(u=""+t),r.key!==void 0&&(u=""+r.key),"key"in r){t={};for(var n in r)n!=="key"&&(t[n]=r[n])}else t=r;return r=t.ref,{$$typeof:a,type:R,key:u,ref:r!==void 0?r:null,props:t}}return e.Fragment=l,e.jsx=s,e.jsxs=s,e}var x;function p(){return x||(x=1,i.exports=d()),i.exports}var v=p();function c(){return v.jsx("button",{className:"bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600",children:"¡Haz clic aquí!"})}export{c as default};
